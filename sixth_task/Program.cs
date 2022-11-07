6d8834d098ed742df2e5ffe0a929add9e10616d3
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-07 16:51:58 +0300

    Решение: Задача 6

---
+++ sixth_task/Program.cs
@@ -0,0 +1,20 @@
+﻿// Даны два действительных числа x и у. Вычислить их сумму, разность, произведение и частное.
+
+double x = new Random().NextDouble();
+double y = new Random().NextDouble();
+
+Console.WriteLine("Какие числа мы вычисляем ");
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+Console.Write("Сумма чисел =");
+Console.WriteLine(x + y);
+
+Console.Write("Разность чисел =");
+Console.WriteLine(x - y);
+
+Console.Write("Произведение чисел =");
+Console.WriteLine(x * y);
+
+Console.Write("Частное чисел =");
+Console.WriteLine(x / y);
