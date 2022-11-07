dec73de0cb6303fde7a45bf3bec82435ee0d0856
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-07 07:57:37 +0300

    Решение: Задача 1

---
+++ first_task/Program.cs
@@ -0,0 +1,7 @@
+﻿// Составить программу вывода на экран в одну строку четырех любых чисел с тремя пробелами между ними.
+
+int a = new Random().Next(1,123);
+int b = new Random().Next(4,667);
+int c = new Random().Next(12,90);
+int d = new Random().Next(56,99);
+Console.WriteLine($"{a}   {b}   {c}   {d}");
