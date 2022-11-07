9e5342631f66e23cb2bb44b628413bb3a54bad2a
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-07 10:06:37 +0300

    Решение: Задача 4

--- fourth_task/Program.cs
+++ fourth_task/Program.cs
@@ -1,3 +1,7 @@
 ﻿// Составить программу вывода на экран числа, вводимого с клавиатуры. Выводимому числу должно предшествовать сообщение «Вы ввели число».
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt16(Console.ReadLine());
 
+Console.Write("Вы ввели число: ");
+Console.WriteLine(number);
