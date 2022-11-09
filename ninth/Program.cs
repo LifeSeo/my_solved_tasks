06a7014283d31ac36f3b4ac2d61e9c4c09428f9d
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-09 11:42:48 +0300

    Решение: Задача 9 (Друзья и собака)

---
+++ ninth/Program.cs
@@ -0,0 +1,35 @@
+﻿// Решение задачи про двух друзей и собаку. Необходимо найти, сколько раз собака успеет пробежать между друзьями.
+
+using System;
+class Program {
+  static void Main()
+{
+float count = 0;
+float distance = 10000000;
+float firstFriendSpeed = 1;
+float secondFriendSpeed = 2;
+float dogSpeed = 5;
+float friend = 2;
+float time = 0;
+
+
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+}
+Console.WriteLine($"Собака пробежит: {count} раз");
+  }
+}
