493d033b5b08aea01e71e9193c76c53ab570403b
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-11 15:26:08 +0300

    домашнее задание (задача 4): 11 ноября 2022

---
+++ twelfth_task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное
+// из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+
+Console.WriteLine($"Максимальное число: {max}");
