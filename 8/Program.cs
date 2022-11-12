33123e15b9a36b4f6eb7366fa8369a56549e4081
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-12 18:01:48 +0300

    Необходимо ввести имя с клавиатуры и попроветствовать пользователя. Для пользователя Маша, необходимо специальное приветствие

---
+++ 8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Необходимо ввести имя с клавиатуры и попроветствовать пользователя. Для пользователя "Маша", необходимо специальное приветствие.
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Вав, Маша, наконец ты пришла");
+}
+else
+{
+Console.WriteLine($"Привет, {username}");    
+}
+
+
