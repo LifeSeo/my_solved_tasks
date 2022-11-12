ea726237633aaf5e6158dadbc9da0e6ddee60064
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-12 09:28:26 +0300

    Находим максимальное число использую функцию

---
+++ 15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Находим максимальное число использую функцию
+
+int Max(int num1, int num2, int num3)
+{
+    int res = 0;
+    if (num1 > res) res = num1;
+    if (num2 > res) res = num2;
+    if (num3 > res) res = num3;
+return res;
+}
+int GetDate()
+{
+    Console.Write("Введите значение: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int max = Max(
+    Max(GetDate(), GetDate(), GetDate()),
+    Max(GetDate(), GetDate(), GetDate()),
+    Max(GetDate(), GetDate(), GetDate())
+);
+Console.WriteLine(max);
