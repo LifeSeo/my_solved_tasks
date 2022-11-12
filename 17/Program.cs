18f1bf838fd0fcb8689ded12bcce8233695b892e
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-12 13:55:39 +0300

    Найдём элемент массива, совпадающий с некоторым значением, который определяет пользователь

---
+++ 17/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найдём элемент массива, совпадающий с некоторым значением,
+// который определяет пользователь.
+
+int[] array = { 45, 67, 12, 56, 456, 76, 99, 678, 9869 };
+int n = array.Length;
+int find = 99;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    else
+    {
+        index++;
+    }
+}
