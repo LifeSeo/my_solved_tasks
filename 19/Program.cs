e5ee5c17cf471c9917a97e10c151a0e5ca75696b
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-12 17:36:25 +0300

    Задайте массив и отсортируйте его от меньшего к большему

---
+++ 19/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задайте массив и отсортируйте его от меньшего к большему
+
+int[] array = new int[] {23,90,-4,12,-99,78,3,34};
+        Array.Sort(array);
+        Console.WriteLine("Выводим значения: ");
+        Array.ForEach(array, Console.WriteLine);
