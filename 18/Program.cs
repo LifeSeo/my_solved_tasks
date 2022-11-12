c004efddd13370e74896b9d1082c9dcf3f700929
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-12 16:02:05 +0300

    Заполнить массив рандомно целыми числами, вывести номер индекса и значения каждого элемента

---
+++ 18/Program.cs
@@ -0,0 +1,12 @@
+﻿// Заполнить массив рандомно целыми числами, вывести номер индекса и значения каждого элемента.
+
+int[] array = new int[9];
+Random rand = new Random();
+
+for (int x = 0; x < array.Length; x++)
+{
+    array[x] = rand.Next(99);
+    Array.Sort(array);
+
+    Console.WriteLine("Индекс элемента массива " + x + " Число в массиве = " + array[x]);
+}
