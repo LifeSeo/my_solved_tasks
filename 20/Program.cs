1cc705fd15b02363681c520fc0bc3f34c4ddc0aa
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-13 18:31:08 +0300

    Создайте функцию и с ее помощью выведите на экран консоли сегодняшнею дату

---
+++ 20/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создайте функцию и выведите на экран консоли сегодняшнею дату
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Programm x = new Programm();
+        x.Date();
+    }
+
+void Date()
+{
+    DateTime todayDate = DateTime.Now;
+    Console.WriteLine(todayDate.ToString("dd/MM/yyyy"));
+}
+}
