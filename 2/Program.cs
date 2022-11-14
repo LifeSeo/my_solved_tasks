c301730f2dcff4f36d9c3eebbd69676a454d19fb
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-14 10:47:42 +0300

    Составить программу вывода на экран «столбиком» пяти любых чисел, введенных пользователем с клавиатуры.

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,4 +1,4 @@
-﻿//Составить программу вывода на экран «столбиком» пяти любых чисел, введенных пользователем с клавиатуры. 
+﻿// Составить программу вывода на экран «столбиком» пяти любых чисел, введенных пользователем с клавиатуры. 
 
 Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
@@ -8,7 +8,7 @@ Console.WriteLine("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите четвертое число: ");
 int d = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите пятое число" );
+Console.WriteLine("Введите пятое число");
 int e = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Ответ на поставленную задачу:");
@@ -17,4 +17,3 @@ Console.WriteLine(b);
 Console.WriteLine(c);
 Console.WriteLine(d);
 Console.WriteLine(e);
-        
