9298f425f26ae5be58a7bda31a03925108bcfa2c
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-14 10:41:13 +0300

    Найти наибольшее число в массиве с помощью цикла FOR

---
+++ 21/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти наибольшее число в массиве с помощью цикла FOR
+
+int[] array = new int[] { 75, 7798, 2573, 76, 685, -570, 4762, 586, -6378, 26 };
+int max = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+
+}
+Console.WriteLine(max);
