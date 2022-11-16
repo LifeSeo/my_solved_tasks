8985bb041e088335dd088cc630fa4ba058a28018
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-16 11:14:18 +0300

    Найти максимальную цифру из рандомного трехзначного числа используя функцию

--- 28/Program.cs
+++ 28/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Найти максимальную цифру из рандомного трехзначного числа используя функцию
 
-int number = new Random().Next(100,999);
+int number = new Random().Next(100, 999);
 Console.WriteLine($"Найти максимальную цифру из --> {number}");
 
 // int number1 = number / 100;
@@ -14,14 +14,14 @@ Console.WriteLine($"Найти максимальную цифру из --> {num
 
 int MaxDigit(int number)
 {
-int number1 = number / 100;
-int number2 = number % 100 / 10;
-int number3 = number % 10;
-int max = 0;
-if (number1 > max) max = number1; 
-if (number2 > max) max = number2;
-if (number3 > max) max = number3;
-return max;
+    int number1 = number / 100;
+    int number2 = number % 100 / 10;
+    int number3 = number % 10;
+    int max = 0;
+    if (number1 > max) max = number1;
+    if (number2 > max) max = number2;
+    if (number3 > max) max = number3;
+    return max;
 }
 int maxDigit = MaxDigit(number);
 
