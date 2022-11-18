d0f45a81aa0cc28cb6f0a4a513263cf91d6b1478
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-18 15:12:16 +0300

    Домашняя работа. Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ 36/Program.cs
@@ -0,0 +1,23 @@
+﻿// Домашняя работа. Задача 23. Напишите программу, которая принимает
+// на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Quarto()
+{
+    int count = 0;
+    while (count <= n)
+    {
+    Console.WriteLine($"Число {count} в кубе --> {count * count * count}");
+    count++;
+    }
+}
+Quarto();
+
