0072fd997986875a0d98f31a1093907708173481
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-19 14:16:05 +0300

    Решение через строку и массив. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом

---
+++ 38/Program.cs
@@ -0,0 +1,13 @@
+﻿// Решение через строку и массив. Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string strNumber = Convert.ToString(number);
+
+bool StrNumber(string num)
+{
+return strNumber[0] == strNumber[4] && strNumber[1] == strNumber[3];
+}
+
+Console.WriteLine(StrNumber(strNumber) ? "Является палиндромом" : "Не является палиндромом");
