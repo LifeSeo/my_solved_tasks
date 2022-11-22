7855a66af7af99242846a4bce6a6649d1c97a449
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-22 13:02:18 +0300

    Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе

---
+++ 41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Digits(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count == 0 ? 1 : count;
+}
+
+if (number < 0) number = -number;
+int result = Digits(number);
+Console.WriteLine($"Количество цифр в числе {number} ---> {result}");
