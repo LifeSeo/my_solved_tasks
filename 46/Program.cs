65f5cdc580506ff33bd639dbef25a4f0e4131685
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-22 17:16:31 +0300

    Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран

---
+++ 46/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и
+// выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2707);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+RandomArray(array);
+
+PrintArray(array);
