660e1a63148ccc04ddb8d1e46863ecffe9b4cda5
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-22 13:05:27 +0300

    Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А

---
+++ 40/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 24: Напишите программу, которая
+// 1. принимает на вход число (А) и 
+// 2. выдаёт сумму чисел от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+
+    return sum;
+}
+
+if (number > 0)
+{
+    int sumDigits = SumDigits(number);
+    Console.WriteLine($"Сумма чисел от 1 до {number} = {sumDigits}");
+}
+else System.Console.WriteLine("Требуется ввести натуральное число");
