870f1d3e1b17216a8df58278f81801334fa1ada1
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-25 15:43:19 +0300

    Домашняя работа. Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве

---
+++ 50/Program.cs
@@ -0,0 +1,44 @@
+﻿// Домашняя работа. Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] NewArrayRandom(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+Console.Write("Выводим массив на печать: ");
+int[] array = NewArrayRandom(5, 100, 999);
+PrintArray(array);
+
+int EvenNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int evenNumber = EvenNumber(array);
+Console.WriteLine($"Количество четных чисел в массиве ----> {evenNumber}");
