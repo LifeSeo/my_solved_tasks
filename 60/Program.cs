7413a555aac6e190b735a866f0b812fe794bb95f
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-11-29 13:08:38 +0300

    Напишите программу, которая будет преобразовывать десятичное число в двоичное

---
+++ 60/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int[] Binary(int numb)
+{
+    int numb1 = numb;
+    int digits = 0;
+    while (numb1 > 0)
+    {
+        numb1 /= 2;
+        digits += 1;
+
+    }
+    int[] bin = new int[digits];
+    for (int i = 0; i < bin.Length; i++)
+    {
+        bin[bin.Length-i-1] = numb % 2;
+        numb /= 2;
+    }
+    return bin;
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] binary = Binary(number);
+// Console.WriteLine(Binary(number));
+PrintArray(binary, "", "", "");
