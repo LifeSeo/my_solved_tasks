bbd88153927dee002594272f4464609320510a46
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-12-02 12:55:04 +0300

    Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.

---
+++ 67/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+            // TODO: вывести индексы массива.
+            // matrix[i, j] = i * 10 + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string beginRow, string separatorElems, string endRow)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write(beginRow);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],4}{separatorElems}");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(endRow);
+    }
+}
+
+// int SumDiagonalMatrix(int[,] matrix)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i == j) sum += matrix[i, j];
+//         }
+//     }
+//     return sum;
+// }
+
+int SumDiagonalMatrix(int[,] matrix)
+{
+    int sum = 0;
+    int minIndex = matrix.GetLength(0);
+    if (matrix.GetLength(1) < matrix.GetLength(0)) minIndex = matrix.GetLength(1);
+    for (int i = 0; i < minIndex; i++)
+        sum += matrix[i, i];
+    return sum;
+}
+
+int[,] array = CreateMatrixRndInt(4, 3, 1, 9);
+PrintMatrix(array, "", "", "");
+Console.WriteLine();
+int sumDiagonalMatrix = SumDiagonalMatrix(array);
+Console.Write($"Сумма элементов, находящихся на главной диагонали = {sumDiagonalMatrix}");
