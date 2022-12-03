6d09cd08e4c7935eb1fe5ae34f414b1ed4ad8977
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-12-03 16:49:16 +0300

    дан двумерный массив. пределить: сумму всех элементов третьей строки массива

---
+++ 71/Program.cs
@@ -0,0 +1,52 @@
+﻿// Дан двумерный массив. Определить:
+// сумму всех элементов третьей строки массива;
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int SummaElementsMatrixRows(int[,] matrix)
+{
+    int summa = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == 2)
+            summa += matrix[i, j];
+        }
+    }
+    return summa;
+}
+
+void PrintMatrix(int[,] matrix)
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+
+int[,] array = CreateMatrixRndInt(4, 3, 1, 10);
+PrintMatrix(array);
+int summaElementsMatrixRows = SummaElementsMatrixRows(array);
+Console.WriteLine(summaElementsMatrixRows);
