35deefe27bbf3b2e814e8325049c548a35dbffa1
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-12-09 13:09:14 +0300

    Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр

---
+++ 84/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int sumNum = SumNumb(number);
+Console.Write(sumNum);
+int SumNumb(int num)
+{
+    int sum = 0;
+    if (num % 10 == 0) return sum;
+    sum = sum + num % 10 + SumNumb(num/10);
+    return sum;
+}
