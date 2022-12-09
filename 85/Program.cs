5676323197baf545a24eb2ec590168aff58c49f2
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-12-09 13:18:18 +0300

    Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.

---
+++ 85/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((DegreeNumber(a,b)));
+
+int DegreeNumber(int a, int b)
+{
+    if (b == 0) return 1;
+    return DegreeNumber(a, b - 1) * a;
+}
+
