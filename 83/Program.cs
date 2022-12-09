cac3f6d7b633112e44d22d99f3e24159d74ae3bd
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-12-09 13:11:18 +0300

    Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N

---
+++ 83/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(number1, number2);
+void NaturalNumbers(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        NaturalNumbers(num1, num2 + 1);
+        Console.Write($"{num2} ");
+    }
+    if (num1 < num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 + 1, num2);  
+    }
+    if (num1 == num2)
+        Console.Write($"{num1} ");
+}
+
+
+
