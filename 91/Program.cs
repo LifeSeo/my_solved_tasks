88c49a21603534e622a1753531ca95d3c95bd092
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-12-12 14:55:11 +0300

    Заменить четные строки наоборот в рандомной двухмерной матрице

---
+++ 91/Program.cs
@@ -0,0 +1,58 @@
+﻿// Заменить четные строки наоборот в рандомной двухмерной матрице
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] ChangeRowsMatrix(int[,] matrix)
+{
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    if (j < k)
+                    {
+                        int temp = matrix[i, j];
+                        matrix[i, j] = matrix[i, k];
+                        matrix[i, k] = temp;
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
+}
+int[,] matrix = CreateMatrixRndInt(5, 5, 1, 21);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] changeRowsMatrix = ChangeRowsMatrix(matrix);
+PrintMatrix(changeRowsMatrix);
