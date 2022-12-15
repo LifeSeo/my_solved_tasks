4c1064404a5b77849ba85d0ec5169f598f6a7e40
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2022-12-15 13:20:25 +0300

    Создать треугольник Паскаля, значение строк задает пользователь

---
+++ 94/Program.cs
@@ -0,0 +1,35 @@
+﻿// Создать треугольник Паскаля, значение строк задает пользователь
+
+Console.WriteLine("Введите количество строк треугольника Паскаля: ");
+double strings = Convert.ToInt32(Console.ReadLine());
+
+double Factorial(double strings)
+{
+    double result = 1;
+    for (int i = 1; i < strings; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+void PrintTriangle()
+{
+    for (int i = 0; i < strings; i++)
+    {
+        for (int j = 0; j <= (strings - i); j++)
+        {
+            Console.Write(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write("   ");
+            Console.Write(Factorial(i) / (Factorial(j) * Factorial(i - j)));
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+PrintTriangle();
+
