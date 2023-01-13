8b6a769af8d550f4c9568797daf417017c79d5b4
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-01-13 14:53:56 +0300

    Добавили тестовые обьекты на экран игры

--- First_game/main.py
+++ First_game/main.py
@@ -6,8 +6,22 @@ pygame.display.set_caption("Моя первая игрушка на Python")
 icon = pygame.image.load("First_game/images/icon.png")
 pygame.display.set_icon(icon)
 
+squire = pygame.Surface((50, 170))
+squire.fill("Red")
+
+my_font = pygame.font.Font("First_game/fonts/Oswald.ttf", 40)
+text_surface = my_font.render("Game", True, "Green")
+
+player = pygame.image.load("First_game/images/golf.png")
+
 running = True
 while running:
+    
+    screen.blit(squire, (0, 0))
+    screen.blit(text_surface, (250, 50))
+    screen.blit(player, (50, 100))
+    pygame.draw.circle(screen, "Blue", (250, 150), 30)
+    # pygame.draw.circle(squire, "Blue", (10, 7), 5)
     # screen.fill((22, 177, 247))
     pygame.display.update()
     for even in pygame.event.get():
