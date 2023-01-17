d8fcbc623c0e99923c53e1036da41441b8424673
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-01-17 13:37:43 +0300

    Добавили автоматическое появление монстров по таймеру, а так же точки соприкосновения с главным героем

--- First_game/main.py
+++ First_game/main.py
@@ -4,7 +4,7 @@ clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((618, 359))
 pygame.display.set_caption("Моя первая игрушка на Python")
-icon = pygame.image.load("First_game/images/icon.png")
+icon = pygame.image.load("First_game/images/icon.png").convert_alpha()
 pygame.display.set_icon(icon)
 
 # squire = pygame.Surface((50, 170))
@@ -14,24 +14,26 @@ pygame.display.set_icon(icon)
 # text_surface = my_font.render("Game", True, "Green")
 
 # player = pygame.image.load("First_game/images/golf.png")
-back_ground = pygame.image.load("First_game/images/fon_background.jpg")
+back_ground = pygame.image.load("First_game/images/fon_background.jpg").convert_alpha()
 
 # player = pygame.image.load('First_game/images/animation/left_person/left_1.png')
 
 walk_left = [
-    pygame.image.load('First_game/images/animation/left_person/left_1.png'),
-    pygame.image.load('First_game/images/animation/left_person/left_2.png'),
-    pygame.image.load('First_game/images/animation/left_person/left_3.png'),
-    pygame.image.load('First_game/images/animation/left_person/left_4.png')
+    pygame.image.load('First_game/images/animation/left_person/left_1.png').convert_alpha(),
+    pygame.image.load('First_game/images/animation/left_person/left_2.png').convert_alpha(),
+    pygame.image.load('First_game/images/animation/left_person/left_3.png').convert_alpha(),
+    pygame.image.load('First_game/images/animation/left_person/left_4.png').convert_alpha()
 ]
 
 walk_right = [
-    pygame.image.load('First_game/images/animation/right_person/right_1.png'),
-    pygame.image.load('First_game/images/animation/right_person/right_2.png'),
-    pygame.image.load('First_game/images/animation/right_person/right_3.png'),
-    pygame.image.load('First_game/images/animation/right_person/right_4.png')
+    pygame.image.load('First_game/images/animation/right_person/right_1.png').convert_alpha(),
+    pygame.image.load('First_game/images/animation/right_person/right_2.png').convert_alpha(),
+    pygame.image.load('First_game/images/animation/right_person/right_3.png').convert_alpha(),
+    pygame.image.load('First_game/images/animation/right_person/right_4.png').convert_alpha()
 ]
 
+ghost = pygame.image.load("First_game/images/ghost.png").convert_alpha()
+# ghost_x = 620
 player_anime_count = 0
 bg_second = 0
 
@@ -39,11 +41,15 @@ bg_sound = pygame.mixer.Sound('First_game/sounds/bg.mp3')
 bg_sound.play(loops= -1)
 volume = 1.0
 
+ghost_timer = pygame.USEREVENT +1
+pygame.time.set_timer(ghost_timer, 2500)
+ghost_list_in_game = []
+
 player_speed = 5
 player_x = 150
 player_y = 220
 is_jump = False
-jump_count = 7
+jump_count = 8
 running = True
 while running:
     
@@ -52,6 +58,18 @@ while running:
     # screen.blit(back_ground, (0, 0))
     screen.blit(back_ground, (bg_second, 0))
     screen.blit(back_ground, (bg_second + 618, 0))
+    # screen.blit(ghost, (ghost_x, 220))
+    
+    player_rest = walk_left[0].get_rect(topleft =(player_x, player_y))
+    # ghos_rect = ghost.get_rect(topleft =(ghost_x, 220))
+    
+    if ghost_list_in_game:
+        for el in ghost_list_in_game:
+            screen.blit(ghost, el)
+            el.x -= 10
+    
+            if player_rest.colliderect(el):
+                print("Вы проиграли")
     
     keys = pygame.key.get_pressed()
     
@@ -69,7 +87,7 @@ while running:
         if keys[pygame.K_SPACE]:
             is_jump = True
     else:
-        if jump_count >= -7:
+        if jump_count >= -8:
             if jump_count > 0:
                 player_y -= (jump_count ** 2) / 2
             else:
@@ -77,7 +95,9 @@ while running:
             jump_count -= 1
         else:
             is_jump = False
-            jump_count = 7
+            jump_count = 8
+            
+    # ghost_x -= 10
     
     if player_anime_count == 3:
         player_anime_count = 0
@@ -96,6 +116,8 @@ while running:
         if even.type == pygame.QUIT:
             running = False
             pygame.quit()
+        if even.type == ghost_timer:
+                ghost_list_in_game.append(ghost.get_rect(topleft =(620, 230)))
         elif even.type == pygame.KEYDOWN:
             if even.key == pygame.K_F1:
                 screen.fill((52, 134, 235))
@@ -109,6 +131,6 @@ while running:
                 volume += 0.1
                 bg_sound.set_volume(volume)
                 print(bg_sound.get_volume() )
-                
+            
                 
     clock.tick(15)
