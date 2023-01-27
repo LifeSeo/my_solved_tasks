8ec75755afd867805e07075597948879df7b22c2
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-01-27 15:35:10 +0300

    Задача 2: Найдите сумму цифр трехзначного числа

---
+++ 123/main.py
@@ -0,0 +1,19 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+def Integer():
+    while True:
+        try:
+            number = int(input("Введите трехзначное число: "))
+        except ValueError:
+            print("Вы ввели НЕ число!")
+
+        else:
+            if len(str(number)) == 3:
+                return print(sum(map(int, str(number))))
+            else:
+                print("Вы ввели НЕ трехзначное число!")
+                number = int(input("Введите трехзначное число: "))
+                
+    
+Integer()
+
