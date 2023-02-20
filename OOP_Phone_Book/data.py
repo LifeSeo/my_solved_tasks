acebf9cc1b43e9ebaf2721c820d77c1a5a062a6c
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-02-20 16:49:41 +0300

    ООП Телефонная книга (доработанная версия)

--- OOP_Phone_Book/data.py
+++ OOP_Phone_Book/data.py
@@ -1,11 +1,19 @@
 def print_data(data):
     print(data)
-    
+
+
 def input_data():
     answer = ""
-    data = input("Записать данные (нажмите 1) | Найти данные (нажмите 2): ")
+    data = input("Записать данные (нажмите 1) | Найти данные (нажмите 2)\n"
+                 "Изменить данные (нажмите 3) | Удалить данные (нажмите 4): ")
     if data == "1":
         answer = "1"
+    elif data == "2":
+        answer = "2"
+    elif data == "3":
+        answer = "3"
+    elif data == "4":
+        answer = "4"
     else:
-        answer = input("По каким данным произвести поиск: ")
+        print("Вы не верно ввели номер нужного действия!")
     return answer
--- OOP_Phone_Book/put_in_file.py
+++ OOP_Phone_Book/put_in_file.py
@@ -4,11 +4,43 @@ import data
 def put_file(input_data):
     if input_data == "1":
         dict = open("OOP_Phone_Book\phone_dictionary.txt", "a+")
+        dict.write(input("Введите ФИО и телефон через пробел:\n").upper())
         dict.write("\n")
-        dict.write(input("Введите Фамилию Имя Отчество и телефон через пробел: "))
         return dict
-    else:
+    
+    elif input_data == "2":
+        find_word = input("Введите данные, которые требуется найти: ").upper()
         dict = open("OOP_Phone_Book\phone_dictionary.txt", "r")
         for i in dict:
-            if input_data.lower() in i.lower():
+            if find_word in i.upper():
                 return i
+    elif input_data == "3":
+        find_data = input("Какие данные необходимо изменить: ").upper()
+        dict = open("OOP_Phone_Book\phone_dictionary.txt", "r")
+        for i in dict:
+            if find_data in i.upper():
+                change_data = input("На что будем менять: ").upper()
+                dict = open("OOP_Phone_Book\phone_dictionary.txt", "r")
+                new_dict_data = dict.read().replace(find_data, change_data)
+                dict = open("OOP_Phone_Book\phone_dictionary.txt", "w")
+                dict.write(new_dict_data)
+                return new_dict_data
+    elif input_data == "4":
+        del_date = input("Что требуется удалить: ").upper()
+        dict = open("OOP_Phone_Book\phone_dictionary.txt", "r")
+        for i in dict:
+            if del_date in i.upper():
+                del_date_str = input("Удалить всю строку:1, Удалить только слово:2: ")
+                if del_date_str == "1":
+                    dict = open("OOP_Phone_Book\phone_dictionary.txt", "r")
+                    new_dict_data = dict.read().replace(i, "")
+                    dict = open("OOP_Phone_Book\phone_dictionary.txt", "w")
+                    dict.write(new_dict_data)
+                    return new_dict_data
+                else:
+                    dict = open("OOP_Phone_Book\phone_dictionary.txt", "r")
+                    new_dict_data = dict.read().replace(del_date, "")
+                    dict = open("OOP_Phone_Book\phone_dictionary.txt", "w")
+                    dict.write(new_dict_data)
+                    return new_dict_data
+        dict.close()
