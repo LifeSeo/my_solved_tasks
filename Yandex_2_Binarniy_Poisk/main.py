828933a55ae1e3162ecbad8454c6807c1feb85de
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-02-21 13:08:57 +0300

    Требуется найти в бинарном векторе самую длинную последовательность единиц и вывести её длину

---
+++ Yandex_2_Binarniy_Poisk/main.py
@@ -0,0 +1,24 @@
+# Требуется найти в бинарном векторе самую длинную последовательность единиц
+# и вывести её длину.
+
+# Желательно получить решение, работающее за линейное время и при этом
+# проходящее по входному массиву
+# только один раз.
+
+from random import randint as rnd
+
+binan_vector = list(map(lambda _: rnd(0, 1), range(10)))
+
+
+count = 0
+max = 0
+for i in binan_vector:
+    print(i, end="---")
+    if i == 1:
+        count += 1
+
+    else:
+        if count > max:
+            max = count
+        count = 0
+print("Единица повторяется: ", max if max > count else count)
