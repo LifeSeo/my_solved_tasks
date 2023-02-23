dddad42a61425ff69bdc5c2deb04212b19637c4a
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-02-23 18:03:59 +0300

    HomeWork, Task 40, 42(new version)

--- 173/main.py
+++ 173/main.py
@@ -1,16 +1,19 @@
 import pandas as pa
-
+print("\n")
 file = pa.read_csv(r'173\california_housing_train.csv')
-
+print("---------------Выводим с 0 по 9 индексы таблицы (10 значений)-----------------\n")
 print(file.head(10))
-
-print("------------------")
 
 # Задача 40: Работать с файлом california_housing_train.csv, который находится в папке
 # sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
-
-print(file[file["population"] <= 500].median())
+print("\n")
+print("---------------Средняя стоиомсть дома с населением до 500------------------\n")
+middle_price = file[file["population"] <= 500].median()
+print(middle_price.medianHouseValue) 
+# сделал через переменную, интересно было посмотреть, как это работает
 
 # Задача 42: Узнать какая максимальная households в зоне минимального значения population
-print("------------------")
+print("\n")
+print("---------------Максимальная households в зоне минимального значения population------------------\n")
 print((file["households"].max()) & (file["population"].min()))
+print("\n")
