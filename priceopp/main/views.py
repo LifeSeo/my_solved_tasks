7117c732a60e07749d3d508f848cc05d3b3b0c9c
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-03-09 11:21:38 +0300

    Создали шаблон на основе Jinja и убрали повторяющийся код

--- priceopp/main/views.py
+++ priceopp/main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Тест - мордочка сайта')
+    return render(request, 'main/index.html')
+
+def about(request):
+    return HttpResponse('<h4>Тест - О нас</h4>')
