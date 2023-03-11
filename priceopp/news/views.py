271f81e8f55e6535b356b844447592c59df23c9a
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-03-11 16:01:07 +0300

    Добавили расположение новостей в блоке News

--- priceopp/news/views.py
+++ priceopp/news/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Articles
 
 def news_home(request):
-    return render(request, 'news/news.html')
+    news = Articles.objects.order_by('date')[:3]
+    return render(request, 'news/news.html', {'news' :news})
