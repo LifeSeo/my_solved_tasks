212ee2c6ebc2567f91d91806691dbf001c7ec411
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-03-24 15:23:53 +0300

    Добавили Logout и отображение статуса авторизации

--- priceopp/accounts/urls.py
+++ priceopp/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
@@ -7,5 +8,5 @@ urlpatterns = [
     path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    
+    path('logout/', LogoutView.as_view(template_name = 'registration/logout.html'), name='logout'),
 ]
