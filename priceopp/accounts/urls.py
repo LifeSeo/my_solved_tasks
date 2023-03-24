f4421ad5e7f5c2af622ecd3bf7155252112a9257
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-03-24 13:39:31 +0300

    Добавили форму авторизации

--- priceopp/accounts/urls.py
+++ priceopp/accounts/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
     path('', views.signup, name='signup'),
     path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    
 ]
