20db523d81fce575b314922c4b000e2cfffcc8c3
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-06-05 14:32:37 +0300

    Cделана возможность редактирования и удаления данных

--- Notebook/main.py
+++ Notebook/main.py
@@ -26,4 +26,37 @@ def input_date():
                 print(i)
             else:
                 print("Данные не найдены")
+    elif answer == "3":
+         find_data = input("Какие данные необходимо изменить: ").upper()
+         dict = open("Notebook/notebook.txt", "r", encoding="utf-8")
+         for i in dict:
+            if find_data in i.upper():
+                change_data = input("На что будем менять: ").upper()
+                dict = open("Notebook/notebook.txt", "r", encoding="utf-8")
+                new_dict_data = dict.read().replace(find_data, change_data)
+                dict = open("Notebook/notebook.txt", "w", encoding="utf-8")
+                dict.write(new_dict_data)
+
+            else:
+                print("Данные не найдены")
+    elif answer == "4":
+        del_date = input("Что требуется удалить: ").upper()
+        dict = open("Notebook/notebook.txt", "r", encoding="utf-8")
+        for i in dict:
+            if del_date in i.upper():
+                del_date_str = input("Удалить всю строку:1, Удалить только слово:2: ")
+                if del_date_str == "1":
+                    dict = open("Notebook/notebook.txt", "r", encoding="utf-8")
+                    new_dict_data = dict.read().replace(i, "")
+                    dict = open("Notebook/notebook.txt", "w", encoding="utf-8")
+                    dict.write(new_dict_data)
+                    return new_dict_data
+                else:
+                    dict = open("Notebook/notebook.txt", "r", encoding="utf-8")
+                    new_dict_data = dict.read().replace(del_date, "")
+                    dict = open("Notebook/notebook.txt", "w", encoding="utf-8")
+                    dict.write(new_dict_data)
+                    return new_dict_data
+        dict.close()
+    
     
