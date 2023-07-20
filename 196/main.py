d1e1b0eee36acb9ea4639dbaa8856fcf6b6663a9
LifeSeo <116150774+LifeSeo@users.noreply.github.com>
2023-07-20 17:40:26 +0300

    Метод пирамидальной сортировки

---
+++ 196/main.py
@@ -0,0 +1,17 @@
+from heapq import heappop, heappush  
+   
+def heapsort(list1):  
+     heap = []  
+     for ele in list1:  
+         heappush(heap, ele)  
+   
+     sort = []  
+   
+     # the elements are lift in the heap  
+     while heap:  
+         sort.append(heappop(heap))  
+   
+     return sort  
+   
+list1 = [14335, 7608, -7599, 34, 95, 111, 56, 0, 99, 67, 53, 11]  
+print(heapsort(list1))
